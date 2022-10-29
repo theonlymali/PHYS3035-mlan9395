4bdc8b9be4b1da42c987243f2778a7c822dc9ecb
theonlymali <116885291+theonlymali@users.noreply.github.com>
2022-10-29 16:54:19 +1100

    Add files via upload

--- PHYS3035_vacuum_cross_section.m
+++ PHYS3035_vacuum_cross_section.m
@@ -2,41 +2,33 @@
 %-----------------------------------------------------------------------
 % Script %
 %-----------------------------------------------------------------------
-a = 0.01:0.00001:1; %m
-m = zeros(1,length(a));
-f = 2.08*10^9; %Hz
-c = 2.998*10^8; %m/s
-for i = 1:length(a)
-    m(i) = int64(2*a(i)*f/c);
-end
-funa = []; %m
-for i = 1:length(a)
-    if m(i) == 1
-        funa = [funa,a(i)];
-    end
-end
-b = zeros(1,length(funa)); %m
-amp = 0.034;
-per = 2*0.0721; %1/s
-for i = 1:length(funa)
-    b(i) = -amp*cos(2*pi/per*funa(i));
+a = 0.0721; %m
+b = 0.034; %m
+x = 0:0.00001:a; %m
+y = zeros(1,length(x)); %m
+amp = b; %m
+per = 2*a; %1/s
+for i = 1:length(x)
+    y(i) = amp*sin((2*pi/per)*x(i));
 end
 %%
 %-----------------------------------------------------------------------
 % Plotting %
 %-----------------------------------------------------------------------
+xlim([0, 0.0721])
+ylim([0, 0.034])
 line_width = 2;
 axes_fontsize = 24;
 set(0,'DefaultAxesFontSize',axes_fontsize)
 set(0,'defaultAxesFontName','Times')
 f = figure(1);
     f.Color = "white"; hold on
-p = plot(funa,b,'Color','r','LineWidth',line_width);
+p = plot(x,y,'Color','r','LineWidth',line_width);
 formatSpec = 'm = 1' ;
 A_sub = sprintf(formatSpec,1);
 title(A_sub,'FontWeight','Normal')    
-xlabel("Side Length $a$ (metres)",'interpreter','latex')
-ylabel("Side Length $b$ (metres)",'interpreter','latex')
+xlabel("$x$ (metres)",'interpreter','latex')
+ylabel("$y$ (metres)",'interpreter','latex')
 formatSpec = '%.0e' ;
 A_min_leg = sprintf(formatSpec,min(a));
 A_max_leg = sprintf(formatSpec,max(a));
